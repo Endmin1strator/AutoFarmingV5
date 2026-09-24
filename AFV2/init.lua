3f6f919344ac54d064e8969fa9c006ef5b78b2bf
Endm1strator <zeroschth38@gmail.com>
2026-09-24 23:07:34 +0700

    Update init.lua

--- AFV2/init.lua
+++ AFV2/init.lua
@@ -15,7 +15,7 @@
 --//   loadstring(game:HttpGet(".../AFV2/init.lua"))()
 
 local BRANCH = "main"
-local BASE = "https://raw.githubusercontent.com/Endmin1strator/AutoFarmingV/refs/heads/master/"
+local BASE = "https://raw.githubusercontent.com/Endmin1strator/AutoFarmingV5/refs/heads/master/"
 	.. BRANCH .. "/AFV2/"
 
 --// Concatenation order. This list is the single source of truth for it.
